236e054e50c679bb441b684a3b47976db5730bd7
Dave Brosius <dbrosius@mebigfatguy.com>
2018-06-10 15:41:12 -0400

    fix test

--- src/test/java/com/mebigfatguy/asmstack/ParameterStackMethodVisitorTest.java
+++ src/test/java/com/mebigfatguy/asmstack/ParameterStackMethodVisitorTest.java
@@ -8,7 +8,8 @@ public class ParameterStackMethodVisitorTest {
     @Test
     public void testSimpleReturn() {
 
-        ParameterStackMethodVisitor v = new ParameterStackMethodVisitor(Opcodes.ASM6);
+        ParameterStackMethodVisitor v = new ParameterStackMethodVisitor(Opcodes.ASM6, Opcodes.ACC_PUBLIC, "test", "()Ljava/lang/Object;",
+                "()Ljava/lang/Object;", new String[0]);
         v.visitCode();
 
         v.visitVarInsn(Opcodes.ALOAD, 1);
